3fc30e078d93bf053c2760a661b0b3c437fa87e3
Ayzaan Wahid <ayzaanwahid@gmail.com>
2012-08-07 21:31:42 -0700

    Update main.js to make graph prettier

--- site/main.js
+++ site/main.js
@@ -127,7 +127,7 @@
   }    
 
   $(document).ready(function(){
-    var sys = arbor.ParticleSystem(1000, 600, 0.5) // create the system with sensible repulsion/stiffness/friction
+    var sys = arbor.ParticleSystem(1000, 0, 0.5) // create the system with sensible repulsion/stiffness/friction
     sys.parameters({gravity:true}) // use center-gravity to make the graph settle nicely (ymmv)
     sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
 

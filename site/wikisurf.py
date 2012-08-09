484375c142439580d141f15ee31e20b6b08a6a43
Ayzaan Wahid <ayzaan@ayzaan.(none)>
2012-08-08 23:41:02 -0700

    Fix index.html

--- site/wikisurf.py
+++ site/wikisurf.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from parse import *
+import networkx as nx
 
 app = Flask(__name__)
 
@@ -6,5 +8,16 @@ app = Flask(__name__)
 def main_page():
 	return render_template('index.html')
 
+@app.route('/<article>/<depth>')
+def get_article(article, depth):
+	center_page_title = article.encode('utf-8')
+	graph_depth = int(depth)
+
+	graph = get_graph(center_page_title, graph_depth)
+	graph_json = output_json(graph)
+
+	return jsonify(graph_json)
+	
+
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
